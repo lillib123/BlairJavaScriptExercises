973074d9276367e915ad9c1897542cc71d84aff4
lillib123 <lblair2@emich.edu>
2017-10-04 21:30:19 -0400

    script update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 1)
-var a = Number(prompt("An nth Triangle Number is n + n+1. For example, with n=5, the triangular number would be 0+1+2+3+4+5, or 15. Enter a number to triangulate."));
+var a = prompt("An nth Triangle Number is n + n+1. For example, with n=5, the triangular number would be 0+1+2+3+4+5, or 15. Enter a number to triangulate.");
 var sum = 0;
 while (Number.isInteger(a) === false) {
 var a = Number(prompt("Please enter an integer to triangulate."));
@@ -15,18 +15,18 @@ for (var i=0; i<=a; i++) {
 // 2)
 var sentence = (" ");
 do {
-  var yesorno = String(prompt("Do you want to play?"));
+  var yesorno = prompt("Do you want to play?");
   if (yesorno === "no") {
     break;
   }
   while (yesorno != "yes" && yesorno != "no") {
-    var yesorno = String(prompt("Do you want to play? Enter yes or no."));
+    var yesorno = prompt("Do you want to play? Enter yes or no.");
     if (yesorno === "no") {
       break;
     }
   }
   if (yesorno === "yes") {
-    var wordtoadd = String(prompt("Enter a word."));
+    var wordtoadd = prompt("Enter a word.");
     var sentence = (sentence + " " + wordtoadd);
   }
   else if (yesorno === "no") {
@@ -38,18 +38,18 @@ while (yesorno = "yes");
 
 
 //3
-var yesorno = String(prompt("Would you like to print your name?"));
+var yesorno = prompt("Would you like to print your name?");
 if (yesorno != "yes") {
   console.log("Have a lovely day.");
 }
 if (yesorno === "yes") {
-  var name = String(prompt("Enter your name."));
+  var name = prompt("Enter your name.");
   console.log(name);
-  var again = String(prompt("Would you like to print your name AGAIN?"))
+  var again = prompt("Would you like to print your name AGAIN?");
   while (again === "yes") {
     var name = (name + "!");
     console.log(name);
-    var again = String(prompt("Would you like to print your name AGAIN?"))
+    var again = prompt("Would you like to print your name AGAIN?");
   }
   if (again != "yes") {
     console.log("Thanks for playing!");
@@ -57,9 +57,9 @@ if (yesorno === "yes") {
 }
 
 //4
-var time = String(prompt("What time of day is it?"));
+var time = prompt("What time of day is it?");
 while (time != "morning" || time != "noon" || time != "evening") {
-  var time = String(prompt("What time of day is it?"));
+  var time = prompt("What time of day is it?");
   if (time === "morning" || time=== "noon" || time=== "evening") {
     break;
   }
